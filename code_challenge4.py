f3674ad7b24a62921f1e57474c7ca3be462364c2
Yeasha Mei P. Diño <yeashameidino137@gmail.com>
2024-12-06 08:11:42 +0800

    Add files via upload
    Code Challenge 3-4

---
+++ code_challenge4.py
@@ -0,0 +1,28 @@
+num1 = eval(input("\n\t Enter the first number: "))
+
+num2 = eval(input("\t Enter the second number: "))
+
+sum = num1 + num2
+
+dif = num1 - num2
+
+pro = num1 * num2
+
+quo = num1 / num2
+
+exp = num1 ** num2
+ 
+rem = num1 % num2
+
+flrdv = num1 // num2
+
+
+print("\n\n\t The sum of", num1, "and", num2, "is", sum, "\b. \n\t The difference of", num1, "and", num2, "is", dif, "\b. \n\t The product of", num1, "and", num2, "is", pro, "\b. \n\t The quotient of", num1, "and", num2, "is", quo, "\b.\n\t", num1, "exponent by", num2, "is", exp, "\b.\n\t The remainder of", num1, "and", num2, "is", rem, "\b.\n\t The floor division of", num1, "and", num2, "is", flrdv, "\b.")
+
+
+
+
+
+
+
+
